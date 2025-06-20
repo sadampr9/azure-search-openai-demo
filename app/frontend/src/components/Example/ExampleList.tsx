7811964a2a52db0f85a5fe077efda4a6ae6b8eff
sadampr9 <59670048+sadampr9@users.noreply.github.com>
2025-06-19 19:48:01 -0500

    Update ExampleList.tsx

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -11,7 +11,7 @@ interface Props {
 export const ExampleList = ({ onExampleClicked, useGPT4V }: Props) => {
     const { t } = useTranslation();
 
-    const DEFAULT_EXAMPLES: string[] = [t("defaultExamples.1"), t("defaultExamples.2"), t("defaultExamples.3")];
+    const DEFAULT_EXAMPLES: string[] = ["¿Cuánto tiempo demora el trámite de constitución?", "¿Qué tipos de sociedades puedo constituir en Perú?", "¿Cómo reservo el nombre de mi empresa?"];
     const GPT4V_EXAMPLES: string[] = [t("gpt4vExamples.1"), t("gpt4vExamples.2"), t("gpt4vExamples.3")];
 
     return (
